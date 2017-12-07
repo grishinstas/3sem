036780b9b214e70839bc09ac91f69a86184cdc79
grishinstas <32033391+grishinstas@users.noreply.github.com>
2017-12-07 21:02:04 +0300

    Update 4_raise_self.c

--- task6/examples/4_raise_self.c
+++ task6/examples/4_raise_self.c
@@ -1,16 +1,43 @@
 #include <signal.h>
 #include <stdio.h>
-
-void handler(int sig) {
-    printf("HANDLED\n");
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+void handler1(int sig, char *a, char *n) {
+    *n *= 2;
+}
+void handler2(int sig, char *a, char *n) {
+    *a += *n;
+    *n *= 2;
 }
  
-void main() {
-    //register the SIGINT handler
-    signal(SIGINT, handler);
-    printf("begin\n");
-    raise(SIGINT);
-    printf("end\n");
-    printf("Note that: the execution wasn't interrupted: the handler is alternative\n");
-    return;
-}
+int main() {
+    pid_t pid = fork();
+    if (pid) {
+        pid = getpid() + 1;
+        int fd = open("a.txt", O_RDONLY);
+        while(read(fd, *a, 1) > 0)
+            for(n = 1; n; n *= 2)
+                if(a & n)
+                    kill(pid, SIGUSR1);
+                else
+                    kill(pid, SIGUSR2);
+        close(fd);
+        kill(pid, SIGKILL);
+        return 0;
+    }
+    else {
+        signal(SIGUSR1, handler1);
+        signal(SIGUSR2, handler2);
+        char *a, *n;
+        *a = 0;
+        *n = 1;
+        open("b.txt", O_WRONLY | O_CREAT | O_TRUNC);
+        while(1)
+            if (*n == 0) {
+                write(fd, a, 1);
+                *a = 0;
+                *n = 1;
+            }
+        return 0;
+    }
