dac1a69c60d73d5d49def96b61dac28045ffed69
grishinstas <32033391+grishinstas@users.noreply.github.com>
2017-12-14 18:22:15 +0300

    Update 4_raise_self.c

--- task6/examples/4_raise_self.c
+++ task6/examples/4_raise_self.c
@@ -15,14 +15,14 @@ int main() {
     pid_t pid = fork();
     if (pid) {
         pid = getpid() + 1;
-        int fd = open("a.txt", O_RDONLY);
+        int fd1 = open("a.txt", O_RDONLY);
         while(read(fd, *a, 1) > 0)
             for(n = 1; n; n *= 2)
                 if(a & n)
                     kill(pid, SIGUSR2);
                 else
                     kill(pid, SIGUSR1);
-        close(fd);
+        close(fd1);
         kill(pid, SIGKILL);
         return 0;
     }
@@ -32,10 +32,10 @@ int main() {
         char *a, *n;
         *a = 0;
         *n = 1;
-        open("b.txt", O_WRONLY | O_CREAT | O_TRUNC);
+        int fd2 = open("b.txt", O_WRONLY | O_CREAT | O_TRUNC);
         while(1)
             if (*n == 0) {
-                write(fd, a, 1);
+                write(fd2, a, 1);
                 *a = 0;
                 *n = 1;
             }
