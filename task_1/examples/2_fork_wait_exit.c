02314896b49f1ce81953fbaec1c19890882f4d49
grishinstas <32033391+grishinstas@users.noreply.github.com>
2017-12-17 17:34:58 +0300

    Update 2_fork_wait_exit.c

--- task_1/examples/2_fork_wait_exit.c
+++ task_1/examples/2_fork_wait_exit.c
@@ -1,47 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int argc, char **argv, char *envp)
 {
-	while(1)
-	{
-		const pid_t pid = fork();
-	if (pid < 0) {
-		printf("fork error\n");
-		return -1;
-		}
-	if (pid) {
-		int status;
-		waitpid(pid, &status, 0);
-		write(stdout, WEXITSTATUS(status), sizeof(int));
-		} else {
-			char path[255], args[4096], pathargs[4096];
-			int i;
-			for(i = 0; i < 255; i++)
-				args[i] = path[i] = 0;
-			for(i = 255; i < 4096; i++)
-				args[i] = 0;
-			scanf("%s", pathargs);
-			i = 0;
-			while(pathargs[i] != ' ') {
-				path[i] = pathargs[i];
-				i++;
-			}
-			i++;
-			int j = 0;
-			while(pathargs[i]) {
-				args[j] = pathargs[i];
-				i++;
-				j++;
-			}
-			execvp(path, args);
-			printf("exec error\n");
-			return -1;
-		}
-	}
-	return 0;
+                        char path[255], pathargs[4096];
+                        int i;
+                        if(argc > 1) {
+                                pid_t pid = fork();
+                                if (pid) {
+                                        int status;
+                                        waitpid(pid, &status, 0);
+                                        printf("ret code=%d\n", WEXITSTATUS(status));
+                                }
+                                else {
+                                execvp(argv[1], argv + 1);
+                                printf("exec error");
+                                return -1;
+                                }
+                        }
+                                while(1) {
+                                char pathargs[256];
+                                char vector[256][256];
+                                char *pointer[256];
+                                printf("Enter the command or press Ctrl-C for exit>");
+                                gets(pathargs);
+                                int i, j, k;
+                                i = j = 0;
+                                while(pathargs[i] != '\0') {
+                                        k = 0;
+                                        while (pathargs[i] != ' ' && pathargs[i] != '\0') {
+                                                vector[j][k] = pathargs[i];
+                                                i++;
+                                                k++;
+                                        }
+                                        vector[j][k] = '\0';
+                                        pointer[j] = vector[j];
+                                        j++;
+                                        if(pathargs[i] != '\0')
+                                                i++;
+                                        }
+                                pointer[j] = (char *) NULL;
+                                pid_t pid = fork();
+                                if (pid) {
+                                        int status;
+                                        waitpid(pid, &status, 0);
+                                        printf("ret code=%d\n", WEXITSTATUS(status));
+                                }
+                                else {
+                                execvp(*pointer, pointer);
+                                printf("exec error\n");
+                                return -1;
+                                }
+        }
+        return 0;
 }
