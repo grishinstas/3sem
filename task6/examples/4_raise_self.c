4daab75f2c9ca5c6bc9596ee1f3834f14a363100
grishinstas <32033391+grishinstas@users.noreply.github.com>
2017-12-18 18:21:16 +0300

    Update 4_raise_self.c
    Побитовая передача файла от дочернего процесса к родительскому

--- task6/examples/4_raise_self.c
+++ task6/examples/4_raise_self.c
@@ -3,44 +3,54 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-void handler1(int sig, char *a, char *n) {
-    *n *= 2;
+#include <stdlib.h>
+char a = 0;
+int n = 1;
+char *aa = &a;
+int *nn = &n;
+void handler1(int sig) {
+        *nn *= 2;
 }
-void handler2(int sig, char *a, char *n) {
-    *a += *n;
-    *n *= 2;
+void handler2(int sig) {
+   *aa += *nn;
+    *nn *= 2;
 }
 int main() {
-    pid_t pid = fork();
-    if (pid) {
-        pid = getpid() + 1;
-        int fd1 = open("a.txt", O_RDONLY);
         int fd2 = open("b.txt", O_WRONLY | O_CREAT | O_TRUNC);
-        while(read(fd1, *a, 1) > 0) {
-            for(n = 1; n; n *= 2)
+         pid_t pid = fork();
+        if(pid < 0) {
+                printf("fork error");
+                exit(-1);
+        }
+        if (pid == 0) {
+        int fd1 = open("a.txt", O_RDONLY);
+        while(read(fd1, &a, sizeof(char)) > 0) {
+            n = 1;
+            while(n != 256) {
                 if(a & n)
-                    kill(pid, SIGUSR2);
+                    kill(getppid(), SIGUSR2);
                 else
-                    kill(pid, SIGUSR1);
+                    kill(getppid(), SIGUSR1);
+                n *= 2;
+        raise(SIGSTOP);
+           }
         }
         close(fd1);
         close(fd2);
-        kill(pid, SIGKILL);
+        kill(getppid(), SIGKILL);
         return 0;
     }
     else {
         signal(SIGUSR1, handler1);
         signal(SIGUSR2, handler2);
-        char *a, *n;
-        *a = 0;
-        *n = 1;
-        while(1)
-            if (*n == 0) {
-                kill(getppid(), SIGSTOP);
-                write(fd2, a, 1);
-                *a = 0;
-                *n = 1;
-                kill(getppid(), SIGCONT);
+        while(1) {
+            if (*nn == 256) {
+                 write(fd2, aa, sizeof(char));
+               *aa = 0;
+                *nn = 1;
             }
+            kill(pid, SIGCONT);
+        }
         return 0;
     }
+}
